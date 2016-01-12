e67a3dbfdfbf5b8483283b094f0995010ab8baf4
Jake VanderPlas <jakevdp@gmail.com>
2016-01-11 16:58:05 -0800

    add robust version

--- lpproj/lpproj.py
+++ lpproj/lpproj.py
@@ -8,21 +8,22 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 class LocalityPreservingProjection(BaseEstimator, TransformerMixin):
     def __init__(self, n_neighbors=5, n_components=2, eigen_solver='auto',
-                 neighbors_algorithm='auto', kernel_width=None):
+                 neighbors_algorithm='auto', kernel_width=None, robust=True):
         self.n_neighbors = n_neighbors
         self.n_components = n_components
         self.eigen_solver = eigen_solver
         self.neighbors_algorithm = neighbors_algorithm
         self.kernel_width = kernel_width
+        self.robust = robust
 
     def fit(self, X, y=None):
+        # TODO: make this more efficient
         X = check_array(X)
         self.nbrs_ = NearestNeighbors(n_neighbors=self.n_neighbors,
                                       algorithm=self.neighbors_algorithm)
         self.nbrs_.fit(X)
         self.training_data_ = self.nbrs_._fit_X
 
-        # TODO: make this more efficient
         if self.kernel_width is None:
             W = kneighbors_graph(self.nbrs_, self.n_neighbors,
                                  mode='connectivity', include_self=True)
@@ -36,14 +37,23 @@ class LocalityPreservingProjection(BaseEstimator, TransformerMixin):
         D = np.diag(W.sum(1))
         L = D - W
         
-        # TODO: Make this computation more stable using
-        # http://www.iipl.fudan.edu.cn/~zhangjp/literatures/MLF/TR-2002-09.pdf
-        A = np.dot(X.T, np.dot(L, X))
-        B = np.dot(X.T, np.dot(D, X))
+        # Compute L' and D' and solve the
+        # generalized eigenvalue problem L' a = v D' a
+        Lp = np.dot(X.T, np.dot(L, X))
+        Dp = np.dot(X.T, np.dot(D, X))
+        eigvals = (0, self.n_components - 1)
         
-        evals, evecs = linalg.eigh(A, B)            
-        self.projection_ = evecs[:, :self.n_components]
+        if not self.robust:
+            evals, evecs = linalg.eigh(Lp, Dp, eigvals=eigvals)
+        else:
+            S, U = linalg.eigh(Dp)
+            S[S <= 0] = np.inf
+            Sm1_2 = 1. / np.sqrt(S)
+            W = Sm1_2[:, None] * np.dot(U.T, np.dot(Lp, U)) * Sm1_2
+            evals, evecs = linalg.eigh(W, eigvals=eigvals)
+            evecs = np.dot(U, Sm1_2[:, None] * evecs)
 
+        self.projection_ = evecs
         return self
 
     def transform(self, X):
